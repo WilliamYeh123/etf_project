8741c9a9028a8f5a0493133ff680e11bd53fb26d
WilliamYeh <WilliamYeh@billows.com.tw>
2024-03-05 15:25:35 +0800

    feat:old code

--- app.py
+++ app.py
@@ -12,10 +12,18 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
 
 app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("config.ini")
+
+
+parser = WebhookParser(config['line_bot']['Channel_Secret'])
 
 line_bot_api = LineBotApi(config['line_bot']['Channel_Access_Token'])
 handler = WebhookHandler(config['line_bot']['Channel_Secret'])
@@ -23,27 +31,43 @@ handler = WebhookHandler(config['line_bot']['Channel_Secret'])
 # client_secret = config['imgur_api']['Client_Secret']
 # album_id = config['imgur_api']['Album_ID']
 # API_Get_Image = config['other_api']['API_Get_Image']
+ETF_id = ['1','2']
 
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    # print("body:",body)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'ok'
-
-
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+ 
+        try:
+            events = parser.parse(body, signature)  # 傳入的事件
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+ 
+        for event in events:
+            if isinstance(event, MessageEvent):  # 如果有訊息事件
+                if event.message.text == 'hello': #查看服務
+                    line_bot_api.reply_message(  
+                    event.reply_token,
+                    TextSendMessage(text='選擇基金:\n1 : 國內成分證券ETF \n2 : 國外成分證券ETF')
+                )
+                # elif event.message.text in ETF_id:
+                #     etf_id = int(event.message.text)
+                #     reply = fl.fiveline(etf_id)
+                #     line_bot_api.reply_message(  
+                #     event.reply_token,
+                #     TextSendMessage(text=reply)
+                # )
+                # else:
+                #     line_bot_api.reply_message(  # 回復傳入的訊息文字
+                #     event.reply_token,
+                #     TextSendMessage(text='error')
+                # )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
